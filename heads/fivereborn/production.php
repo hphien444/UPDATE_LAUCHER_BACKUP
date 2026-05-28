b8482ffc3dc0ff3466aeb7c51386f5f87869bc81
hphien444 <hphien444@users.noreply.github.com>
2026-05-28 08:22:47 +0700

    Publish launcher bootstrap single exe update

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
 header('x-amz-meta-bootstrap-version: 1000016');
-header('x-amz-meta-bootstrap-size: 5085820');
-header('x-amz-meta-bootstrap-object: a6906be51f7952097d158b7899836fb57f741c7a71a5366e4822fcfd68c47449');
-header('x-amz-meta-branch-version: 1779930850');
+header('x-amz-meta-bootstrap-size: 5085572');
+header('x-amz-meta-bootstrap-object: 6ab82dedfac125ee772380048ce07d5daf660613a40453e962ee32cba38e0e44');
+header('x-amz-meta-branch-version: 1779931315');
 header('x-amz-meta-branch-manifest: 19edcd4806fbc33debf951103e9f13e46a6066b335f9a4bd78e83aeeca6e4895');
 echo "OK";
 ?>
