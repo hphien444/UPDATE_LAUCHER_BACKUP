f86560e79dab83bebb9fc83e5412e20d2e78f504
hphien444 <hphien444@users.noreply.github.com>
2026-05-28 08:33:36 +0700

    Remove FxDK shortcut from launcher update

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
 header('x-amz-meta-bootstrap-version: 1000016');
-header('x-amz-meta-bootstrap-size: 5085572');
-header('x-amz-meta-bootstrap-object: 6ab82dedfac125ee772380048ce07d5daf660613a40453e962ee32cba38e0e44');
-header('x-amz-meta-branch-version: 1779931315');
+header('x-amz-meta-bootstrap-size: 5085448');
+header('x-amz-meta-bootstrap-object: 626e344ddd8a02b8d64b8d0ca1a3de197fa3b796bb85a7764c44f5ffd96bfd4a');
+header('x-amz-meta-branch-version: 1779932006');
 header('x-amz-meta-branch-manifest: 19edcd4806fbc33debf951103e9f13e46a6066b335f9a4bd78e83aeeca6e4895');
 echo "OK";
 ?>
