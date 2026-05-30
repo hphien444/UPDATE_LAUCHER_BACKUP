43892a75b6515a6e2a4f66339115f704e75d5c46
hphien444 <hphien444@users.noreply.github.com>
2026-05-30 21:54:36 +0700

    Cache Discord authorization prompts

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000019');
 header('x-amz-meta-bootstrap-size: 5086460');
 header('x-amz-meta-bootstrap-object: fdb054ab112e24fa767c4147f58052055f71732be136390d742d0d4372d26031');
-header('x-amz-meta-branch-version: 1780152031');
-header('x-amz-meta-branch-manifest: 01e0bc3098f7aed7fd3d2d7abc57e378b403db4fedcfd6f797a8b75ab30ca1fc');
+header('x-amz-meta-branch-version: 1780152864');
+header('x-amz-meta-branch-manifest: 1f757d5723d83bad544a9d33b34eb0f22fc8071370438833469e34c18c3cae81');
 echo "OK";
 ?>
