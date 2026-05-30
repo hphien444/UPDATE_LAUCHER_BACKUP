d309d241c55e7edb8b95e377368edf1a1c5e3ce8
hphien444 <hphien444@users.noreply.github.com>
2026-05-30 21:35:32 +0700

    Show runtime message box before opening installer link

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
 header('x-amz-meta-bootstrap-version: 1000018');
-header('x-amz-meta-bootstrap-size: 5086260');
-header('x-amz-meta-bootstrap-object: 105d2273b769531353d873e74265da4007ee6bed956fde7836a1ac69bd0db2a0');
-header('x-amz-meta-branch-version: 1780151383');
+header('x-amz-meta-bootstrap-size: 5086788');
+header('x-amz-meta-bootstrap-object: cb8bd1e7d0176fd562a07cadd7f06ab2f02ad5a7942bae9eb134a4f62255e32a');
+header('x-amz-meta-branch-version: 1780151722');
 header('x-amz-meta-branch-manifest: 01e0bc3098f7aed7fd3d2d7abc57e378b403db4fedcfd6f797a8b75ab30ca1fc');
 echo "OK";
 ?>
