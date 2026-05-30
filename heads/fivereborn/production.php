37a74e90458638951be19755247c43f7b17d456b
hphien444 <hphien444@users.noreply.github.com>
2026-05-30 21:40:41 +0700

    Bump bootstrap version for runtime dialog update

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000018');
-header('x-amz-meta-bootstrap-size: 5086788');
-header('x-amz-meta-bootstrap-object: cb8bd1e7d0176fd562a07cadd7f06ab2f02ad5a7942bae9eb134a4f62255e32a');
-header('x-amz-meta-branch-version: 1780151722');
+header('x-amz-meta-bootstrap-version: 1000019');
+header('x-amz-meta-bootstrap-size: 5086460');
+header('x-amz-meta-bootstrap-object: fdb054ab112e24fa767c4147f58052055f71732be136390d742d0d4372d26031');
+header('x-amz-meta-branch-version: 1780152031');
 header('x-amz-meta-branch-manifest: 01e0bc3098f7aed7fd3d2d7abc57e378b403db4fedcfd6f797a8b75ab30ca1fc');
 echo "OK";
 ?>
