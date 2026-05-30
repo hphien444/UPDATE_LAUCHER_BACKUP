7428f4f2aff0c56b36b0d51193e772fbcc30996f
hphien444 <hphien444@users.noreply.github.com>
2026-05-30 21:29:52 +0700

    Open VC runtime installer link after runtime error

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
 header('x-amz-meta-bootstrap-version: 1000018');
-header('x-amz-meta-bootstrap-size: 5086728');
-header('x-amz-meta-bootstrap-object: b26c43e308ee1f24d1bca473b7d25846a729da71967d691bd3d6c25c56736bd0');
-header('x-amz-meta-branch-version: 1780145045');
+header('x-amz-meta-bootstrap-size: 5086260');
+header('x-amz-meta-bootstrap-object: 105d2273b769531353d873e74265da4007ee6bed956fde7836a1ac69bd0db2a0');
+header('x-amz-meta-branch-version: 1780151383');
 header('x-amz-meta-branch-manifest: 01e0bc3098f7aed7fd3d2d7abc57e378b403db4fedcfd6f797a8b75ab30ca1fc');
 echo "OK";
 ?>
