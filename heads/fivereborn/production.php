a1c5792f8fa046e12b1f11e0ecb434bf1b700848
hphien444 <hphien444@users.noreply.github.com>
2026-05-30 17:15:22 +0700

    Fix missing launcher runtime files

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000016');
 header('x-amz-meta-bootstrap-size: 5085448');
 header('x-amz-meta-bootstrap-object: 626e344ddd8a02b8d64b8d0ca1a3de197fa3b796bb85a7764c44f5ffd96bfd4a');
-header('x-amz-meta-branch-version: 1779932006');
-header('x-amz-meta-branch-manifest: 19edcd4806fbc33debf951103e9f13e46a6066b335f9a4bd78e83aeeca6e4895');
+header('x-amz-meta-branch-version: 1780135857');
+header('x-amz-meta-branch-manifest: fd2eae8a85f18eeee6cf725393a6def3728cb7915b9a3c1535de4a3f3b1852a9');
 echo "OK";
 ?>
