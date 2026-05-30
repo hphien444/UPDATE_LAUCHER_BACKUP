ef47ba72e7061603726ccf98a9f82113999361ed
hphien444 <hphien444@users.noreply.github.com>
2026-05-30 17:21:29 +0700

    Bundle VC runtime for CoreRT

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000016');
 header('x-amz-meta-bootstrap-size: 5085448');
 header('x-amz-meta-bootstrap-object: 626e344ddd8a02b8d64b8d0ca1a3de197fa3b796bb85a7764c44f5ffd96bfd4a');
-header('x-amz-meta-branch-version: 1780135857');
-header('x-amz-meta-branch-manifest: fd2eae8a85f18eeee6cf725393a6def3728cb7915b9a3c1535de4a3f3b1852a9');
+header('x-amz-meta-branch-version: 1780136472');
+header('x-amz-meta-branch-manifest: d05e46afe0ff0941b81bf2f58767e9084d41f9b549c18a1183dc2b83d553432d');
 echo "OK";
 ?>
