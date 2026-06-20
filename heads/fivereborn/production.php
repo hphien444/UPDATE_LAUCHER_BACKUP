ccf2a6ad2e303b40b8651ed95d024397dd7f2b12
HIEN <hphien444@gmail.com>
2026-06-21 01:17:21 +0700

    Update bootstrap and UI cache reset

---
+++ heads/fivereborn/production.php
@@ -0,0 +1,8 @@
+<?php
+header('x-amz-meta-bootstrap-version: 1000023');
+header('x-amz-meta-bootstrap-size: 5093448');
+header('x-amz-meta-bootstrap-object: 88634344f2d773458e7c7d3e9241f861a1439b5927e028b1bf06c868f5188d45');
+header('x-amz-meta-branch-version: 1781979337');
+header('x-amz-meta-branch-manifest: d378a5b5433d7527c078b0cdfd3b8a74c0374d12205b4cfa036cc471ff8a0123');
+echo "OK";
+?>
