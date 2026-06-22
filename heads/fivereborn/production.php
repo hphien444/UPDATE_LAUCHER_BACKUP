d8b1e07797ad8f6e6c47ebe754b5d0c2d7793e5c
HIEN <hphien444@gmail.com>
2026-06-22 08:49:16 +0700

    Repack launcher update mirror

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000023');
 header('x-amz-meta-bootstrap-size: 5093588');
 header('x-amz-meta-bootstrap-object: 475fcd7a4c3bd411a30e2aee40c331ebb3a48a6a2a684ebab6fb12a719fa2654');
-header('x-amz-meta-branch-version: 1781981515');
-header('x-amz-meta-branch-manifest: d378a5b5433d7527c078b0cdfd3b8a74c0374d12205b4cfa036cc471ff8a0123');
+header('x-amz-meta-branch-version: 1782092737');
+header('x-amz-meta-branch-manifest: c00207f4671bb1e5ae66e7d402bd1a0dc8dbd012ee4072625123472a3a368459');
 echo "OK";
 ?>
