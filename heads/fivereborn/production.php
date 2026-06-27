aba31b2cfab40bfba53a1b76b2376b47c80d7909
hphien444 <hphien444@gmail.com>
2026-06-27 18:50:34 +0700

    Update backup package: Ctrl+F4 disconnect before exit

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
 header('x-amz-meta-bootstrap-version: 1000024');
-header('x-amz-meta-bootstrap-size: 5094776');
-header('x-amz-meta-bootstrap-object: 4a5b29e6e98a5deb20f981445b9ef3cd17081e9c83dd5c78986ac4c3bb22fd06');
-header('x-amz-meta-branch-version: 1782293875');
-header('x-amz-meta-branch-manifest: d00143865aac70cbb8a90292ffc088a603d486586b3f8c872c5b0a4110abd69c');
+header('x-amz-meta-bootstrap-size: 5094828');
+header('x-amz-meta-bootstrap-object: f32779110abe5c7068a17b3d9b63327db0a4047a2b249c956b90a1e9bfbff017');
+header('x-amz-meta-branch-version: 1782560959');
+header('x-amz-meta-branch-manifest: 1cb014159be0c90e8b78fe7e6cfacfab750b17fcc3179e77eb4917e56e03b1d8');
 echo "OK";
 ?>
