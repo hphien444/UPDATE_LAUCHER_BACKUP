f41eedf029a7034a996182078afe377fa35b9cc8
HIEN <hphien444@gmail.com>
2026-07-22 17:45:32 +0700

    fix: refresh launcher player count

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000030');
 header('x-amz-meta-bootstrap-size: 5096144');
 header('x-amz-meta-bootstrap-object: 5830b94568230f272cc666e4b8e4b2a24e759822968e1b9a2f2793f686abe0cb');
-header('x-amz-meta-branch-version: 1784715061');
-header('x-amz-meta-branch-manifest: a933a9c0aba9732649a4cba79d8152825c2242fe5841f625ce0e20c5c3bf98a3');
+header('x-amz-meta-branch-version: 1784717074');
+header('x-amz-meta-branch-manifest: f4dc13bc911022f1c22ef8556a71fc68b341ad17467439938c4060f5996bf704');
 echo "OK";
 ?>
