f333b2bd38c5bb8117726a246f01d01878885b3c
HIEN <hphien444@gmail.com>
2026-07-22 13:25:37 +0700

    fix: restore legacy updater artwork 1000030

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000029');
-header('x-amz-meta-bootstrap-size: 5093840');
-header('x-amz-meta-bootstrap-object: c8e3e0f2f78b8f947b2352c8aa6554e39dad5180feabc0b330604a3b076e83bc');
-header('x-amz-meta-branch-version: 1784696319');
+header('x-amz-meta-bootstrap-version: 1000030');
+header('x-amz-meta-bootstrap-size: 5096144');
+header('x-amz-meta-bootstrap-object: 5830b94568230f272cc666e4b8e4b2a24e759822968e1b9a2f2793f686abe0cb');
+header('x-amz-meta-branch-version: 1784701271');
 header('x-amz-meta-branch-manifest: 58d6c5ff0ddafb62bb4b818e7dbf393e130e3430625b90b2e0bb0a8a115e9cb4');
 echo "OK";
 ?>
