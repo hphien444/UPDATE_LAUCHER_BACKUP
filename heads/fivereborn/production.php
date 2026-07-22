7b4ece322eed4227953ec97ef32bc05072740bc0
HIEN <hphien444@gmail.com>
2026-07-22 10:33:31 +0700

    fix: prevent bootstrap downgrade loop in 1000028

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000024');
-header('x-amz-meta-bootstrap-size: 5094828');
-header('x-amz-meta-bootstrap-object: f32779110abe5c7068a17b3d9b63327db0a4047a2b249c956b90a1e9bfbff017');
-header('x-amz-meta-branch-version: 1784690985');
+header('x-amz-meta-bootstrap-version: 1000028');
+header('x-amz-meta-bootstrap-size: 5095304');
+header('x-amz-meta-bootstrap-object: 999a017c2f32d6bd7f0abdeb9ae9da8d353fc4dc4fdc4d1f7ae3402d87b1a3c4');
+header('x-amz-meta-branch-version: 1784691167');
 header('x-amz-meta-branch-manifest: f85629654382ad531002cef5731a74ebc9e4d92c45a6836da12a2d71fe92d763');
 echo "OK";
 ?>
