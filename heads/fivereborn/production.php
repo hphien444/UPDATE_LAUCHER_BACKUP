9aead3b643ac7057a976f06095b3481a6da975db
HIEN <hphien444@gmail.com>
2026-07-22 15:06:59 +0700

    feat: publish launcher gaming login UI

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000030');
 header('x-amz-meta-bootstrap-size: 5096144');
 header('x-amz-meta-bootstrap-object: 5830b94568230f272cc666e4b8e4b2a24e759822968e1b9a2f2793f686abe0cb');
-header('x-amz-meta-branch-version: 1784701271');
-header('x-amz-meta-branch-manifest: 58d6c5ff0ddafb62bb4b818e7dbf393e130e3430625b90b2e0bb0a8a115e9cb4');
+header('x-amz-meta-branch-version: 1784707402');
+header('x-amz-meta-branch-manifest: a4e4332b84913c468a91cabf5eea22a2e9feff5e1b0365f27c190f55a3b1b956');
 echo "OK";
 ?>
