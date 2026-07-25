e54e2f58ee23f7ee90d1552d8f2b2c68fda4a858
HIEN <hphien444@users.noreply.github.com>
2026-07-25 22:24:19 +0700

    Fix stuck startup UI and invalid ReShade DLL handling (1000037)

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000036');
-header('x-amz-meta-bootstrap-size: 5103176');
-header('x-amz-meta-bootstrap-object: fc1fe5e9395dc5896e49f87ce95e2141908b410b28d44174eef4f45a7b504bbd');
-header('x-amz-meta-branch-version: 1784991445');
-header('x-amz-meta-branch-manifest: 61fe0b36b656483a23e638a49a4a1e7907375f0752cf6b21734cb5f733b18886');
+header('x-amz-meta-bootstrap-version: 1000037');
+header('x-amz-meta-bootstrap-size: 5102840');
+header('x-amz-meta-bootstrap-object: 774928cc417367ab0b3d48db9b1421c21398a39b72a52d7f6a09ce2b3980008e');
+header('x-amz-meta-branch-version: 1784992637');
+header('x-amz-meta-branch-manifest: 16f07b0158c9a8378d470b359eb96ffb50e8b5f122f0117fd9d4af140c9ec67e');
 echo "OK";
 ?>
