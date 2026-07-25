48645860cae0b3a3a23f42e82d98b82da806ce10
HIEN <hphien444@users.noreply.github.com>
2026-07-25 18:16:29 +0700

    Release launcher bootstrap 1000034

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000033');
-header('x-amz-meta-bootstrap-size: 5101536');
-header('x-amz-meta-bootstrap-object: f052122745e6dfc2bcd8e8b9dbc6793ae91a3aabb2d7076e731a971fb44d15c8');
-header('x-amz-meta-branch-version: 1784970674');
+header('x-amz-meta-bootstrap-version: 1000034');
+header('x-amz-meta-bootstrap-size: 5101476');
+header('x-amz-meta-bootstrap-object: a678b90771fc3f9db1d00253040ad32f4cbd5794b92241f2acaa7358c12898fd');
+header('x-amz-meta-branch-version: 1784977784');
 header('x-amz-meta-branch-manifest: c73f0660d8736cf14c54739873fdfe376f449f1799bf9bd2a9ff1763d0bd13d2');
 echo "OK";
 ?>
