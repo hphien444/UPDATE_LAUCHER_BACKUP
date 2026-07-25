91f354e713efd2274337ba368709b4aa12e0aac0
HIEN <hphien444@users.noreply.github.com>
2026-07-25 22:03:31 +0700

    Fix regular client launch after update (1000036)

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000035');
-header('x-amz-meta-bootstrap-size: 5102332');
-header('x-amz-meta-bootstrap-object: a8d973031049a218e7f82e7ffd924c64d41779ee3a9a2e2ad20b957ed6ae976b');
-header('x-amz-meta-branch-version: 1784987127');
-header('x-amz-meta-branch-manifest: 9e83e6932defb33be49ff3fe27d9758387715619fc39a7d0e15250dfe3510055');
+header('x-amz-meta-bootstrap-version: 1000036');
+header('x-amz-meta-bootstrap-size: 5103176');
+header('x-amz-meta-bootstrap-object: fc1fe5e9395dc5896e49f87ce95e2141908b410b28d44174eef4f45a7b504bbd');
+header('x-amz-meta-branch-version: 1784991445');
+header('x-amz-meta-branch-manifest: 61fe0b36b656483a23e638a49a4a1e7907375f0752cf6b21734cb5f733b18886');
 echo "OK";
 ?>
