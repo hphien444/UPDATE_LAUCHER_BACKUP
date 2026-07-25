340e150afbc7add6c0e1392eae0ab249e81c7796
HIEN <hphien444@users.noreply.github.com>
2026-07-25 16:57:03 +0700

    Fix regular bootstrap version 1000033

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
 header('x-amz-meta-bootstrap-version: 1000033');
-header('x-amz-meta-bootstrap-size: 5101780');
-header('x-amz-meta-bootstrap-object: 79d61b004b323715c274d391ffc18eb54eccdaf5882437a44ab6f8a1606f4438');
-header('x-amz-meta-branch-version: 1784969659');
+header('x-amz-meta-bootstrap-size: 5101536');
+header('x-amz-meta-bootstrap-object: f052122745e6dfc2bcd8e8b9dbc6793ae91a3aabb2d7076e731a971fb44d15c8');
+header('x-amz-meta-branch-version: 1784970674');
 header('x-amz-meta-branch-manifest: c73f0660d8736cf14c54739873fdfe376f449f1799bf9bd2a9ff1763d0bd13d2');
 echo "OK";
 ?>
