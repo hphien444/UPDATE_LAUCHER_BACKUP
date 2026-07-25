cb61e3510ca5a59e173d5fa7a033f5708e9965db
hphien444 <hphien444@users.noreply.github.com>
2026-07-25 13:00:05 +0700

    Publish citizen anti-cheat update 1000032

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
 header('x-amz-meta-bootstrap-version: 1000032');
-header('x-amz-meta-bootstrap-size: 5100288');
-header('x-amz-meta-bootstrap-object: e7d433122be927313d8f61d45846bd6a2d24ede88e91906d69ace8f73d0c16ac');
-header('x-amz-meta-branch-version: 1784737075');
-header('x-amz-meta-branch-manifest: d8f32c6f651b49d0cc282dddaf24bbcdd910ccf250a55a60fff41be8c8c06324');
+header('x-amz-meta-bootstrap-size: 5101700');
+header('x-amz-meta-bootstrap-object: 9865febba11a15aa8a6464c9baa17d5ae75e935da3d87fadda1ea736b7cc67f1');
+header('x-amz-meta-branch-version: 1784958261');
+header('x-amz-meta-branch-manifest: fc664e6f01c7a3a289f9f09604d847cf66ed8c05682299d632d7f2128b6f6d09');
 echo "OK";
 ?>
