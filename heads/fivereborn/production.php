79ecc2f1d6ae55fc2fc76101135fd7e309f62012
HIEN <hphien444@users.noreply.github.com>
2026-07-26 15:47:38 +0700

    Fix client update version display

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000037');
 header('x-amz-meta-bootstrap-size: 5102840');
 header('x-amz-meta-bootstrap-object: 774928cc417367ab0b3d48db9b1421c21398a39b72a52d7f6a09ce2b3980008e');
-header('x-amz-meta-branch-version: 1785053910');
-header('x-amz-meta-branch-manifest: 53baa2f079e7ac749976f6ac50a0ba881ae1c4f0c5ba777303735419a5f1436d');
+header('x-amz-meta-branch-version: 1785055520');
+header('x-amz-meta-branch-manifest: cd93ce7a53e787032bdc25815b73be67ae185978c02a96a80a2607e98c151863');
 echo "OK";
 ?>
