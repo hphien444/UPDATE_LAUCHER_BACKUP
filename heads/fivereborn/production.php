6dfe9adb143f5be1b605b77316a0f0d51b22daaf
HIEN <hphien444@users.noreply.github.com>
2026-07-30 17:36:10 +0700

    Fix anti-cheat screenshot chunk upload

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000037');
 header('x-amz-meta-bootstrap-size: 5102840');
 header('x-amz-meta-bootstrap-object: 774928cc417367ab0b3d48db9b1421c21398a39b72a52d7f6a09ce2b3980008e');
-header('x-amz-meta-branch-version: 1785399431');
-header('x-amz-meta-branch-manifest: 5582282e1a25aa093980f2c22a18f1e6e88f22a33ddf663f2b12250b249b580f');
+header('x-amz-meta-branch-version: 1785407647');
+header('x-amz-meta-branch-manifest: 04096e9b1d36ccc0fd359cf32eccf555cb49251016146b2daab4ffd7923ae640');
 echo "OK";
 ?>
