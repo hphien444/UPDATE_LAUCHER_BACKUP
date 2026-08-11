7d93fdf4bed84965d341d342d85c96352d0ca214
HIEN <hphien444@users.noreply.github.com>
2026-08-11 23:49:30 +0700

    Publish WESTSIDE TEST updater

---
+++ update_launcher_test/heads/fivereborn/test.php
@@ -0,0 +1,8 @@
+<?php
+header('x-amz-meta-bootstrap-version: 1000038');
+header('x-amz-meta-bootstrap-size: 5102524');
+header('x-amz-meta-bootstrap-object: 64af17f314c53bde00b49d78edae9684bcbce6dfa046cbf8ba1404edd1633308');
+header('x-amz-meta-branch-version: 1786466832');
+header('x-amz-meta-branch-manifest: 08a33a9685cdca05b0c9f1f784ea20713984430baf4f22b11c3c47296db77fba');
+echo "OK";
+?>
