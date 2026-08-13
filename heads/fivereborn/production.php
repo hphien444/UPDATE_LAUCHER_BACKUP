141f3c9ec715cd90bea39feb331a027891c74abf
HIEN <hphien444@users.noreply.github.com>
2026-08-13 18:54:52 +0700

    Publish WESTSIDE CITY production bootstrap 1000059

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000051');
-header('x-amz-meta-bootstrap-size: 5108020');
-header('x-amz-meta-bootstrap-object: d1c57653c1127157d5429444669c3f000eed929366c94e434c914d5006acc205');
-header('x-amz-meta-branch-version: 1786518896');
-header('x-amz-meta-branch-manifest: b8cfecc47fee692369760c8b943b6e754b40e1d0aa43f78c479197796bc4e61f');
+header('x-amz-meta-bootstrap-version: 1000059');
+header('x-amz-meta-bootstrap-size: 5108568');
+header('x-amz-meta-bootstrap-object: 59fbe6d0ab2a8559e97b50dadf7feb5aab6642ee7dd6900e7da585e93f446f14');
+header('x-amz-meta-branch-version: 1786620707');
+header('x-amz-meta-branch-manifest: 80fa0a82466917525a266ccd82dbc8c4fce8eb76cb59d614bd516c449be89143');
 echo "OK";
 ?>
