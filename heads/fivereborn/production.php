7b10a0586ef1e7e4d15c8f9bc82048d7e05c2f7a
HIEN <hphien444@users.noreply.github.com>
2026-08-25 02:24:47 +0700

    Publish WESTSIDE CITY client version 1000060

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000059');
-header('x-amz-meta-bootstrap-size: 5108568');
-header('x-amz-meta-bootstrap-object: 59fbe6d0ab2a8559e97b50dadf7feb5aab6642ee7dd6900e7da585e93f446f14');
-header('x-amz-meta-branch-version: 1786620707');
-header('x-amz-meta-branch-manifest: 80fa0a82466917525a266ccd82dbc8c4fce8eb76cb59d614bd516c449be89143');
+header('x-amz-meta-bootstrap-version: 1000060');
+header('x-amz-meta-bootstrap-size: 5108548');
+header('x-amz-meta-bootstrap-object: 94921e18b7a3e43b5a5a572f05172b15e3a83be95c7470c0bc8bfb756952eb23');
+header('x-amz-meta-branch-version: 1787599296');
+header('x-amz-meta-branch-manifest: eddba7f88638948937f8c3e14e6be84ee41212668c4cc7a53fa804065d34b676');
 echo "OK";
 ?>
